89b98a875604b904ed3b91413ac399e56ea1b220
dattm27 <dattran2003.ttn@gmail.com>
2025-09-28 14:15:27 +0700

    feat: add a gif

--- tests/gifportal.js
+++ tests/gifportal.js
@@ -2,10 +2,34 @@ const anchor = require('@coral-xyz/anchor');
 
 const main = async () => {
   console.log("Starting tests ...");
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider);
   const program = anchor.workspace.Gifportal;
-  const tx = await program.rpc.startStuffOff();
+  const baseAccount = anchor.web3.Keypair.generate();
+  const tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId
+    },
+    signers: [baseAccount],
+  });
   console.log("Your transaction signature: ", tx);
+  let account = await program.account.baseAccount.fetch(
+    baseAccount.publicKey
+  );
+  console.log("GIF Count: ", account.totalGifs.toString());
+
+  await program.rpc.addGif({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+    }
+  })
+
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("GIF Count: ", account.totalGifs.toString());
+
+
 }
 
 const runMain = async () => {
